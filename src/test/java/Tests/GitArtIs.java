bd1ebbc8dbdf3f09837ce8397401680678eb503f
shmammadov <mr.shmammadov@gmail.com>
2019-12-31 22:48:33 -0500

    2nd edition

---
+++ src/test/java/Tests/GitArtIs.java
@@ -0,0 +1,7 @@
+package Tests;
+
+public class GitArtIs {
+    public static void main(String[] args) {
+        System.out.println("Gitti art is");
+    }
+}
--- src/test/java/Tests/TestGit.java
+++ src/test/java/Tests/TestGit.java
@@ -1,4 +1,8 @@
 package Tests;
 
 public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("git.art");
+        System.out.println("Gitti art");
+    }
 }
